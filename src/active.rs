415cbeb2ba67163af1685af21d22953950adb036
Kristof Hetzl <kristof.hetzl@gmail.com>
2021-08-23 11:15:40 +0100

    Listener and dummy test

--- src/active.rs
+++ src/active.rs
@@ -5,8 +5,9 @@ use tokio::io::ReadHalf;
 use tokio::io::WriteHalf;
 
 enum SocketHandlerMsg {
-    Send { msg: String },
-    Close,
+    Send { msg: String }, // Send message
+    Close,                // Close on request
+    RemoteClosed,         // Remote endpoint closed the connection
 }
 
 pub type IncomingMsg = String;
@@ -16,34 +17,14 @@ pub struct Socket {
     send_handler: JoinHandle<()>,
     send_tx: mpsc::Sender<SocketHandlerMsg>,
     receive_handler: JoinHandle<()>,
-    receive_rx: broadcast::Receiver<IncomingMsg>,
+    receive_tx: broadcast::Sender<IncomingMsg>,
+    //pub receive_rx: broadcast::Receiver<IncomingMsg>,
 }
 
 impl Socket {
-    pub async fn connect(address: SocketAddr) -> Result<Socket> {
-        let mut stream = TcpStream::connect(&address).await?;
+    pub async fn connect(address: &SocketAddr) -> Result<Socket> {
+        let stream = TcpStream::connect(address).await?;
         start_stream(stream)
-        /*
-                let (r, w) = stream.split();
-
-                let (send_tx, send_rx) = mpsc::channel(32); //FIXME: channel size?
-                let (receive_tx, receive_rx) = broadcast::channel(32); //FIXME: channel size?
-
-                let send_handler = tokio::spawn(async move {
-                    send_handler(send_rx).await;
-                });
-
-                let receive_handler = tokio::spawn(async move {
-                    receive_handler(receive_tx).await;
-                });
-
-                Ok(Socket {
-                    send_handler,
-                    send_tx,
-                    receive_handler,
-                    receive_rx,
-                })
-        */
     }
 
     pub fn try_send(&self) {
@@ -55,6 +36,10 @@ impl Socket {
     pub fn close(&self) {
         self.send_tx.try_send(SocketHandlerMsg::Close);
     }
+
+    pub fn subscribe(&self) -> broadcast::Receiver<IncomingMsg> {
+        return self.receive_tx.subscribe();
+    }
 }
 
 pub struct Listener {
@@ -62,8 +47,8 @@ pub struct Listener {
 }
 
 impl Listener {
-    pub async fn bind(address: SocketAddr) -> Result<Listener> {
-        let listener = TcpListener::bind(&address).await?;
+    pub async fn bind(address: &SocketAddr) -> Result<Listener> {
+        let listener = TcpListener::bind(address).await?;
         Ok(Listener { listener })
     }
 
@@ -73,9 +58,8 @@ impl Listener {
     }
 }
 
-fn start_stream(mut stream: TcpStream) -> Result<Socket> {
+fn start_stream(stream: TcpStream) -> Result<Socket> {
     let (r, w) = tokio::io::split(stream);
-    // let (r, w) = stream.split();
 
     let (send_tx, send_rx) = mpsc::channel(32); //FIXME: channel size?
     let (receive_tx, receive_rx) = broadcast::channel(32); //FIXME: channel size?
@@ -84,15 +68,17 @@ fn start_stream(mut stream: TcpStream) -> Result<Socket> {
         send_handler(w, send_rx).await;
     });
 
+    let receive_tx_to_pass = receive_tx.clone();
     let receive_handler = tokio::spawn(async move {
-        receive_handler(r, receive_tx).await;
+        receive_handler(r, receive_tx_to_pass).await;
     });
 
     Ok(Socket {
         send_handler,
         send_tx,
         receive_handler,
-        receive_rx,
+        receive_tx,
+        //receive_rx,
     })
 }
 
@@ -100,11 +86,15 @@ async fn send_handler(mut w: WriteHalf<TcpStream>, mut send_rx: mpsc::Receiver<S
     loop {
         match send_rx.recv().await {
             Some(SocketHandlerMsg::Send { msg }) => {
+                let string = String::from("msg");
+                let buf = string.as_bytes();
+                w.write(&buf).await.unwrap();
                 // if let Err(err) = stream.write(msg.as_bytes()).await {
                 eprintln!("failed to write to socket");
                 // }
             }
             Some(SocketHandlerMsg::Close) => {
+                // TODO: should shut down receive_handler as well.
                 return ();
             }
             _ => eprintln!("unexpected stuff"),
--- tests/one_to_one.rs
+++ tests/one_to_one.rs
@@ -1,19 +1,96 @@
 #[cfg(test)]
 use std::net::SocketAddr;
+use std::sync::Arc;
+use tokio::sync::broadcast;
+use tokio::sync::mpsc;
+use zfx_socket::active::Listener;
 use zfx_socket::active::Socket;
 use zfx_socket::Error;
+
+#[derive(Debug, Clone)]
+enum TestMessage {
+    Connect,
+    Send,
+    Response,
+}
+
+struct TestServer {
+    send_tx: mpsc::Sender<TestMessage>,
+    receive_rx: broadcast::Receiver<TestMessage>,
+}
+
+impl TestServer {
+    async fn new(addr: &SocketAddr) -> TestServer {
+        let (send_tx, mut send_rx) = mpsc::channel(32); //FIXME: channel size?
+        let (receive_tx, receive_rx) = broadcast::channel(32); //FIXME: channel size?
+
+        let listener = Listener::bind(addr).await.unwrap();
+
+        tokio::spawn(async move {
+            let mut raw_socket = match listener.accept().await {
+                Err(err) => panic!("Listener accept failure due to: {:?}", err),
+                Ok(socket) => socket,
+            };
+
+            let mut listener_receive_rx = raw_socket.subscribe();
+
+            tokio::spawn(async move {
+                // Listener forward loop
+                loop {
+                    match listener_receive_rx.recv().await {
+                        Ok(_msg) => {
+                            receive_tx.send(TestMessage::Response).unwrap();
+                            ()
+                        }
+                        Err(err) => panic!("Socket hung up: {:?}", err),
+                    }
+                }
+            });
+
+            loop {
+                match send_rx.recv().await {
+                    Some(TestMessage::Send) => raw_socket.try_send(),
+                    _ => panic!("unexpected msg"),
+                }
+            }
+        });
+
+        TestServer {
+            send_tx,
+            receive_rx,
+        }
+    }
+}
 
 #[tokio::test]
 async fn failure_to_connect_due_to_no_listener() {
     let target_address = "127.0.0.1:9090";
     let addr = target_address.parse::<SocketAddr>().unwrap();
-    let s = Socket::connect(addr).await;
+    let s = Socket::connect(&addr).await;
     match s {
         Err(Error::IoError(std::io::Error { .. })) => (),
         Err(err) => panic!("Unexpected error: {:?}", err),
         Ok(socket) => {
             socket.close();
-            panic!("Socket shouldn't be ");
+            panic!("Socket shouldn't be able to ");
         }
     }
+}
+
+#[tokio::test]
+async fn simple_server_client_success() {
+    let target_address = "127.0.0.1:9091";
+    let addr = target_address.parse::<SocketAddr>().unwrap();
+
+    let mut server = TestServer::new(&addr).await;
+    let mut client = Socket::connect(&addr).await.unwrap();
+
+    assert_eq!((), client.try_send());
+
+    match server.receive_rx.recv().await {
+        Ok(TestMessage::Response) => (),
+        unexpected => panic!("unexpected message: {:?}", unexpected),
+    }
+
+    assert!(true);
 }
